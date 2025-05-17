72da129a9d34e5161826d9544b630daf22fec2a3
Haridas Pawar <haridaspawar0203@email.com>
2025-05-17 10:33:47 +0530

    Initial portfolio setup with folder structure

---
+++ tailwind.config.ts
@@ -0,0 +1,37 @@
+import type { Config } from "tailwindcss";
+
+export default {
+  content: ["./app/**/{**,.client,.server}/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: [
+          "Inter",
+          "ui-sans-serif",
+          "system-ui",
+          "sans-serif",
+          "Apple Color Emoji",
+          "Segoe UI Emoji",
+          "Segoe UI Symbol",
+          "Noto Color Emoji",
+        ],
+      },
+      colors: {
+        gray: {
+          200: "#D5DAE1",
+        },
+        black: {
+          DEFAULT: "#000",
+          500: "#1D2235",
+        },
+        blue: {
+          500: "#2b77e7",
+        },
+      },
+      boxShadow: {
+        card: "0px 1px 2px 0px rgba(0, 0, 0, 0.05)",
+      },
+    },
+  },
+  plugins: [],
+} satisfies Config;
