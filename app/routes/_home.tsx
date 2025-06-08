d61247c55b5e17b9d5b5c71638ad35ca4f1e777e
Haridas Pawar <haridaspawar0203@email.com>
2025-06-08 12:33:31 +0530

    Done with homepage

--- app/routes/_home.tsx
+++ app/routes/_home.tsx
@@ -7,14 +7,21 @@ export default function Home() {
       <Navbar />
 
       <section className="max-container">
-        <h1 className="head-text ">
-          I'm{" "}
+        <h1 className="head-text mt-40">
+          {" "}
+          Hello, I'm{" "}
           <span className="blue-gradient_text font-semibold drop-shadow">
-            Haridas Pawar,
-          </span>
+            Haridas Pawar
+          </span>{" "}
         </h1>
+        <p className="text-lg mt-10 text-gray-700 text-justify leading-relaxed mb-4">
+          A passionate <b>Full Stack Developer</b> with expertise in modern web
+          technologies. I love creating innovative solutions and turning ideas
+          into reality through clean, efficient code. Always eager to learn new
+          technologies and take on challenging projects.
+        </p>
+        <hr />
       </section>
-
       <Footer />
     </main>
   );
